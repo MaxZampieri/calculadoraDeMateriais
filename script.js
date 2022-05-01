281d3903a40c788ce51c8fecd46b24ff037e9eb6
Max Zampieri <98056019+MaxZampieri@users.noreply.github.com>
2022-04-30 23:30:44 -0300

    function clear input

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ multiplica.addEventListener("click", function(event){
     document.querySelector(".resultado").innerHTML = total.toFixed(2) + " Litros.";
     
 })
+function resetForm(){
+    with (document) {
+        getElementById("select-m").value = "";
+        resultado.textContent = ""
+    }
+}
